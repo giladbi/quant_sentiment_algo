e16e94749e1d08df728af99f9466df0c5b9c1931
vladiki <vlad2mail@gmail.com>
2013-10-23 02:05:37 -0700

    Implemented system for saving articles of the web given an rss feed

--- activities/get_rss_list.js
+++ activities/get_rss_list.js
@@ -6,13 +6,18 @@ with metadata
 */
 
 var request = require('request');
+var fs = require('fs');
 var xml = require('xml2js');
 var processRssList = require('./process_rss_feed');
 
 var CONFIG = require("../config.json");
 
 var rssList = CONFIG.rss_feeds;
+var outputFolder = CONFIG.output_folder;
 
+
+
+//Process takes in a rss feed
 rssList.forEach(function(feed){
 	processRssList.processRssFeed(feed, function(xmlFeed){
 		var xmlParsed = new xml.parseString(xmlFeed, handleXml);
@@ -20,5 +25,39 @@ rssList.forEach(function(feed){
 });
 
 var handleXml = function(err, result) {
-	console.log(result.rss.channel[0].item);
+
+	var rssChannels = result.rss.channel;
+	rssChannels.forEach(function(channel){
+		// this item should usually have 1 item
+
+		var sourceName = channel.title;
+		var articles = channel.item;
+		console.log("Source name: " + sourceName);
+		for(var i=0; i<articles.length; i++){
+			article = articles[i];
+			var articleTitle = article.title[0];
+
+			destPath = '.'+outputFolder+'/'+articleTitle+'.html';
+
+			fetchUrl(article.link[0], destPath, function(err, data){
+
+				fs.writeFile(data[1], data[0], function (err) {
+				  if (err) throw err;
+				  console.log('Succesfully wrote '+data[0].length+' to '+data[1]);
+				});
+			});
+
+		}
+	});
 }
+
+var fetchUrl = function(url, path, callback){
+	urlFile = request(url, function(error, response, body){
+		if(error) return callback(error);
+		console.log("Downloading article from..."+url);
+		body = [body, path];
+		callback(null,body);
+	});
+
+
+};
